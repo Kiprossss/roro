a75b521989946491ee0350d81eab3ce9943994bd
ShitInMyMind <kiproskiriak7@gmail.com>
2026-06-11 23:11:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,290 @@
+const friends = [
+  {
+    name: "Dandadan (Yappa Yappa ho)",
+    pfp: "assets/dandadan.jpg",
+    message: "That one creature that absolutely LOVES interrupting and yappining nonstop while not even carrying what you're telling her. Nothing is getting registered in that bean brain but when you mention a lore reference or game etc her 2 braincells light up and neuron activates just to continue talking for the next couple of hours. Also instead of sitting and trying to prove her point (although most of the time its wrong or just bullshit) she should start sitting and riding her boyfriend instead. She is also the person calling her friends 'wife' and other cute names but at the same time she calls her boyfriend 'bro'. Are we deadass?",
+    birthday: "Happy birthday my little dummy! keep being stupid, it brightens everybody's day.",
+    video: "assets/dan911.mp4",
+    gallery: [
+      "assets/d1.jpg",
+      "assets/d2.jpg",
+      "assets/d3.jpg",
+      "assets/d4.jpg",
+      "assets/d5.jpg",
+      "assets/d6.jpg",
+      "assets/d7.jpg",
+      "assets/d8.jpg",
+      "assets/d9.jpg"
+    ]
+  },
+
+  {
+    name: "Kith (Discord mod)",
+    pfp: "assets/kith.jpg",
+    message: "That one Discord mod that come out from his cave only when the hangout includes his girlfriend. Goated at videogames and is pretty chill. Its the person that has severe autism. Sometimes filled up with energy but sometimes his social battery is dead. Loves running around while singing 'I shit my pants in the middle of Aldi I only went in to get some fucking Garibaldi'. Also he is the main Jew of the friend group.",
+    birthday: "Happy birthday to who is definitely the gremlin of the group. In a good way of course. Keep up the whimsy and when you choose to yap don’t worry I choose to listen.  No matter what happens in the future just know you will never be forgotten",
+    video: "assets/kithkith.mp4",
+    gallery: [
+      "assets/k1.jpg",
+      "assets/k2.jpg",
+      "assets/k3.jpg",
+      "assets/k4.jpg",
+      "assets/k5.jpg",
+      "assets/k6.jpg",
+      "assets/k7.jpg",
+      "assets/k8.jpg",
+      "assets/k9.jpg",
+      "assets/k10.jpg",
+      "assets/k11.jpg",
+      "assets/k12.jpg"
+    ]
+  },
+
+  {
+    name: "Sophie (BBC lover)",
+    pfp: "assets/sophie.jpg",
+    message: "That Romanian bitch that's hobby is to raigebait her own friends. She is the person that came out with the most iconic songs of all time. For example one of her famous songs is 'Fuck me in my hunter eyes' and 'Its where my semen hides'. Her status is: 'Marals official wife' and she always finds a way to get freaky with her.",
+    birthday: "HAPPY BIRTHDAY RORO MOUUU i hope you have the most amazing time being young and sweet and only 17 !!! from the moment danae introduced you to me and mara i always thought you were one of the coolest and most sweetest people EVER. you have impeccable style and taste and i hope we can keep being friends for years to come because you are one of the happiest and most whimsical people i’ve ever met. let’s keep being the extroverts of the group together and hopefully have the bestest and funnest time at lmf together AND every other crowded and chaotic functions😋 keep being one the swaggiest and kindest people in my life for as long as we know eachother so mwa mwa kisses kisses i love you so very much and i wish you the very best in all your future endeavors and dreams and i hope our skibidouthkia manouthkia prospers and has the coolest lore to ever exist ✌️✌️ lots and lots love ",
+    video: "assets/so.mp4",
+    gallery: [
+      "assets/s1.jpg",
+      "assets/s2.jpg",
+      "assets/s3.jpg",
+      "assets/s4.jpg",
+      "assets/s5.jpg",
+      "assets/s6.jpg"
+    ]
+  },
+
+  {
+    name: "Maralaki (Yaoi lover)",
+    pfp: "assets/maralaki.jpg",
+    message: "That one Armenian person that the whole personality and mood changes based on the people and her surroundings. She is the person that brings the freakiness in the group but when she sees Sophie or Roro then from freakiness she goes to staring up porn. Although she is supportive she loves to abuse you sometimes. Plus goated at ragebaiting kids in Roblox.",
+    birthday: "Happy Birthday Roro!! I wish you the BEST 17th birthday, and please, enjoy it as your last year before all the worries of adulthood catch up to you. I genuinely appreciate how much of a thoughtful and kind-hearted friend and overall joyous person you continue to be even in tough conditions. You are always there for me and know that I will also always be there to stand up for you no matter the cost. I never expected us to get this close from a friend group but I’m SO SO glad we did! I hope we continue to make memories as life goes on and look back on our childhood/teenage life and miss how much fun we had. Your presence in my life has been important from the second I realised you were a “weirdo” like me, from the opinions that most the friend group would find odd, you would usually agree with me, and I like to believe that when someone thinks an idea or preference I have is weird I’ll think of you and say “Roro would find this perfectly normal”. Please enjoy life to its fullest, we’ll do all the wild things with the gang, but don’t look back in anger from any of the choices that you will make, as acceptance and the process moving on is what will keep you going. I love you so much roro mou!",
+    video: "assets/mara.mp4",
+    gallery: [
+      "assets/m1.jpg",
+      "assets/m2.jpg",
+      "assets/m3.jpg",
+      "assets/m4.jpg",
+      "assets/m5.jpg",
+      "assets/m6.jpg"
+    ]
+  },
+
+  {
+    name: "Alexis (God bless)",
+    pfp: "assets/alexis.jpg",
+    message: "That one silly dude that became goated for all of us the first second we saw him. That one dude has an awesome personality and character. Although He is a GD addict he is pretty cool. He is just pure perfection. God bless",
+    birthday: "RORO YOU STUPID IDIOT HAPPY BIRTHDAY, we haven't hung out much at all but the time we did was absolutely PEAAAK and I can't wait for more in the future ❤️‍🩹❤️‍🩹❤️‍🩹❤️‍🩹 YIPPIIIEEIE",
+    video: "assets/alexis.mp4",
+    gallery: [
+      "assets/g1.jpg",
+      "assets/g2.jpg",
+      "assets/g3.jpg",
+      "assets/g4.jpg",
+      "assets/g5.jpg",
+      "assets/g6.jpg",
+      "assets/g7.jpg"
+    ]
+  },
+
+  {
+    name: "Max (Femboy)",
+    pfp: "assets/max.jpg",
+    message: "That one Russian femboy that has a huge femboy potential but he is not using it for some reason. He always says some fucked up shit cz he as no filter plus he is never able to go out with the gng cz he is always too busy gooning to other femboys",
+    birthday: "Dearest Roro, Happy Birthday!!!! Aint no way you are already 17! Even though I barely know you? Even though your presence has been very little in my life, the changes you do are drastic. Each memory of you is quite special. Your character, personality and simply your presence is unique. I want to wish you the most amazing birthday with lots of new epic memories and stories to tell. Let this be a new stepping stone to your future life. And please don't make any drunk mistakes... And I am more excited to keep making more fantastic memories together. Let all your ideas and dreams take root. Enjoy this most special day. Happy Birthday!!! ",
+    video: "assets/fv.mp4",
+    gallery: [
+      "assets/f1.jpg",
+      "assets/f2.jpg",
+      "assets/f3.jpg",
+      "assets/f4.jpg",
+      "assets/f5.jpg",
+      "assets/f6.jpg",
+      "assets/f7.jpg"
+    ]
+  },
+
+  {
+    name: "Anastasia (Stusha)",
+    pfp: "assets/anastasia.jpg",
+    message: "That energetic and absolutely stupid girl that loves you with all her heart. She is the definition of extrovert. She loves doing some ridiculous and questionable things. For some people she is goated but for some she is just annoying. She is just a child but in the good way.",
+    birthday: "happy birthday baby girl💗Finally 17!! Rafaela mou thank you for being the amazing friend you are and an absolute icon. I truly hope you have an amazing day on your special day and enjoy the rest of your year being 17!! Cheers to many more memories baby and i hope you leaving school doesnt change things between us(ill make sure). Will miss you loads but know im always here if you need someone to make a bad decision with🤭💗Happy birthday pookie💗",
+    video: "assets/stusha.mp4",
+    gallery: [
+      "assets/a1.jpg",
+      "assets/a2.jpg",
+      "assets/a3.jpg"
+    ]
+  },
+
+  {
+    name: "Kipros (Mavros)",
+    pfp: "assets/kipros.jpg",
+    message: "That one piece of shit that always annoys you and makes your life ass. He always finds a way to annoy you or to make you mad. Whenever youre having a happy or sad time he will always fuck up everything. Althought he is one of the most supportive friends he will always be your #1 hater for life. While everyone will have something sweet to say to you he will be the one that will make you embarrassed :3",
+    birthday: "Happy bday to the most annoying creature to ever exist. Daily I regret my life choices becase of you but Im still happy to have you around. Althought youre a bitch you are still an amazing friend that makes our life full of happiness and unforgetable memories. Happy bday twin :3, sweet 17 (althought mentally youre 3)... pls dont go bald",
+    video: "assets/cv.mp4",
+    gallery: [
+      "assets/c1.jpg",
+      "assets/c2.jpg",
+      "assets/c3.jpg",
+      "assets/c4.jpg",
+      "assets/c5.jpg",
+      "assets/c6.jpg",
+      "assets/c7.jpg",
+      "assets/c8.jpg"
+    ]
+  },
+
+  {
+    name: "Jayden (sausageroll)",
+    pfp: "assets/jayden.jpg",
+    message: "That one person that you cant stop thinking and craving abt. He is the future husband that is addicted on Pokémon (respect) and playing videogames. He is also a talented person with a silly personality and loyal heart",
+    birthday: "Happy birthday to you, my rafaela. Hope you have an absolutely amazing day or have had, depending on when you receive this. Remember, I love you more than anything in this world and I'll never need to or want to replace you. There are many things that I would like to say to you. Always remember, I'll be here for you. Have a good day. I love you so much",
+    video: "assets/JJ.mp4",
+    gallery: [
+      "assets/j1.jpg",
+      "assets/j2.jpg",
+      "assets/j3.jpg",
+      "assets/j4.jpg",
+      "assets/j5.jpg",
+      "assets/j6.jpg",
+      "assets/j7.jpg"
+    ]
+  }
+];
+
+let currentFriend = null;
+const friendsDiv = document.getElementById("friends");
+
+friends.forEach((friend, index) => {
+  const div = document.createElement("div");
+  div.className = "friend";
+  div.innerHTML = `
+    <img src="${friend.pfp}" class="pfp">
+    <span>${friend.name}</span>
+  `;
+  div.onclick = () => openChat(index);
+  friendsDiv.appendChild(div);
+});
+
+function hideAll() {
+  ["home","chat","gallery","birthday"].forEach(id =>
+    document.getElementById(id).classList.add("hidden")
+  );
+}
+
+function openChat(index) {
+  currentFriend = friends[index];
+  hideAll();
+  document.getElementById("chat").classList.remove("hidden");
+
+  chatName.innerText = currentFriend.name;
+  chatMessage.innerText = currentFriend.message;
+  chatPfp.src = currentFriend.pfp;
+}
+
+function goHome() {
+  hideAll();
+  home.classList.remove("hidden");
+  closeVideo();
+}
+
+function goBackToChat() {
+  hideAll();
+  chat.classList.remove("hidden");
+  closeVideo();
+  bdayAudio.pause();
+}
+
+function showVideo() {
+  const container = document.getElementById("videoContainer");
+  const video = document.getElementById("videoPlayer");
+
+  video.pause();
+  video.removeAttribute("src");
+  video.load();
+
+  video.src = currentFriend.video;
+  video.load();
+
+  container.classList.add("show");
+  container.classList.remove("hidden");
+
+  video.play();
+}
+
+function closeVideo() {
+  const container = document.getElementById("videoContainer");
+  const video = document.getElementById("videoPlayer");
+
+  video.pause();
+  video.currentTime = 0;
+  container.classList.remove("show");
+  container.classList.add("hidden");
+}
+
+function openGallery() {
+  hideAll();
+  gallery.classList.remove("hidden");
+
+  const galleryEl = document.getElementById("galleryImages");
+  galleryEl.innerHTML = "";
+
+  currentFriend.gallery.forEach(img => {
+    const image = document.createElement("img");
+    image.src = img;
+    image.onclick = () => window.open(img, "_blank");
+    galleryEl.appendChild(image);
+  });
+}
+
+function openBirthday() {
+  hideAll();
+  birthday.classList.remove("hidden");
+
+  document.getElementById("birthdayText").innerText =
+    currentFriend.birthday;
+
+  bdayAudio.currentTime = 0;
+  bdayAudio.play();
+}
+
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerText = ["💖","✨","🎀","💕"][Math.floor(Math.random()*4)];
+  heart.style.left = Math.random() * 100 + "vw";
+  document.body.appendChild(heart);
+
+  setTimeout(() => heart.remove(), 6000);
+}, 500);
+
+document.addEventListener("click", () => {
+  meowSound.currentTime = 0;
+});
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const btn = document.createElement("button");
+  btn.innerText = "📱 Install App";
+  btn.style.position = "fixed";
+  btn.style.top = "20px";
+  btn.style.right = "20px";
+
+  btn.onclick = async () => {
+    deferredPrompt.prompt();
+    await deferredPrompt.userChoice;
+  };
+
+  document.body.appendChild(btn);
+});
